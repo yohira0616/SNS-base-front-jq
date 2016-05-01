53d252f6a968bb9df2ee2479f555f62427bbc3e8
yohira0616 <yukito.sys.so@gmail.com>
2016-05-01 18:15:11 +0900

    読み込みのテスト実行

--- src/scripts/button-event.js
+++ src/scripts/button-event.js
@@ -3,7 +3,24 @@
   $(function () {
 
     $('#ex-button').click(()=> {
-      alert('clicked!');
+      var SERVER_URL = 'http://localhost:8084';
+      var param={
+        userId:'admin',
+        contents:'test'
+      };
+
+      $.ajax({
+        type: 'POST',
+        url: SERVER_URL + '/api/article/create',
+        data: JSON.stringify(param),
+        contentType: 'application/json',
+        dataType: 'JSON'
+      }).success(function (data) {
+        alert('done!');
+        console.log(data)
+      }).error(function (data) {
+        console.log(data)
+      })
     })
   })
 
