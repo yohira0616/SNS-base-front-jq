f8ff3d59e4feaf75a86634cf898c6ada9c60dcfa
yohira0616 <yukito.sys.so@gmail.com>
2016-05-03 21:59:54 +0900

    fixbug レスポンスが空だとエラー

--- src/scripts/common/ajax-helper.js
+++ src/scripts/common/ajax-helper.js
@@ -1,29 +1,23 @@
-
-
-module.exports=(function ($) {
+module.exports = (function ($) {
   var ajaxHelper = {};
   var SERVER_URL = 'http://localhost:8084';
 
   ajaxHelper.post = function (path, requestParam) {
     return $.ajax({
-        type: 'POST',
-        url: SERVER_URL + path,
-        data: JSON.stringify(requestParam),
-        contentType: 'application/json',
-        dataType: 'JSON'
-      });
+      type: 'POST',
+      url: SERVER_URL + path,
+      data: JSON.stringify(requestParam),
+      contentType: 'application/json'
+    });
   };
 
   ajaxHelper.get = function (path, requestParam) {
     return $.ajax({
-        type: 'GET',
-        url: SERVER_URL + path,
-        data: JSON.stringify(requestParam),
-        contentType: 'application/json',
-        dataType: 'JSON'
-      });
+      type: 'GET',
+      url: SERVER_URL + path,
+      data: JSON.stringify(requestParam),
+      contentType: 'application/json'
+    });
   };
-
   return ajaxHelper;
-
 })(window.jQuery);
