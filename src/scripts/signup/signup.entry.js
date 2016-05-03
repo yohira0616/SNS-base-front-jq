7aaa7810790b0ba783583fd56e7b00c418ca78e9
yohira0616 <yukito.sys.so@gmail.com>
2016-05-03 19:48:08 +0900

    signup実装

--- src/scripts/signup/signup.entry.js
+++ src/scripts/signup/signup.entry.js
@@ -1,3 +1,19 @@
 (function ($) {
-  
+
+  $(function () {
+    $('.app-register-btn').click(function () {
+      var userId = $('#userid').val();
+      var passwd = $('#passwd').val();
+      var email = $('#email').val();
+
+      var createParam = {
+        userId: userId,
+        password: passwd,
+        email: email
+      };
+      console.log(createParam);
+      console.log('登録しました');
+    });
+  })
+
 })(window.jQuery);
