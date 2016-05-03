c1e71994c3e61c4099de1abe9d23d89938949191
yohira0616 <yukito.sys.so@gmail.com>
2016-05-03 20:32:50 +0900

    エラーメッセージ

--- src/scripts/signup/signup.entry.js
+++ src/scripts/signup/signup.entry.js
@@ -1,19 +1,38 @@
 (function ($) {
 
   $(function () {
+
+
     $('.app-register-btn').click(function () {
       var userId = $('#userid').val();
       var passwd = $('#passwd').val();
       var email = $('#email').val();
-
+      //validation
+      if (!userId) {
+        return;
+      }
+      if (!passwd) {
+        return;
+      }
+      if (!email) {
+        return;
+      }
       var createParam = {
         userId: userId,
         password: passwd,
-        email: email
+        emailAddress: email
       };
-      console.log(createParam);
-      console.log('登録しました');
+      let ajaxHelper = require('../common/ajax-helper');
+      ajaxHelper.post('/api/users/create', createParam)
+        .success(function (response) {
+          
+        }).error(function (error) {
+        console.log(error);
+        window.alert('error!');
+      });
+
     });
+
   })
 
 })(window.jQuery);
