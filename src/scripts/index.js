7c6d0bbda4dad568e5e48234609ff542b1fe5e85
yohira0616 <yukito.sys.so@gmail.com>
2016-05-03 16:27:15 +0900

    bootstrap使用

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,16 +1,11 @@
 (function ($) {
   $(function () {
 
-    var contents = 'Hello,jQuery!';
-    //es6 functions can use(ex.template strings)
-    var elem = `<h1>${contents}</h1>`;
-    $('body').append(elem);
     var SERVER_URL = 'http://localhost:8084';
 
     $.get(SERVER_URL + '/api/article/timeline/admin')
       .success(function (data) {
         console.log(data);
       })
-
   })
 })(window.jQuery);
