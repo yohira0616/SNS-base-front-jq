a97963e9cd2b739dd29766ac7061f2656ee4c66a
yohira0616 <yukito.sys.so@gmail.com>
2016-05-07 08:12:04 +0900

    コミュニティの読み込みを実装

--- src/scripts/dashboard/community.js
+++ src/scripts/dashboard/community.js
@@ -4,6 +4,8 @@
 
   $(function () {
 
+    loadCommunities();
+
     $('#app-new-com-save-btn').click(function () {
       let communityName = $('#app-new-com-name').val();
       if (!communityName) {
@@ -28,7 +30,24 @@
      * 保存されているコミュニティ一覧を読み込みます
      */
     function loadCommunities() {
+      ajaxSender.get('/api/community/getComs')
+        .then((res)=> {
+          const $list = $('#app-com-list');
+          $list.empty();
+          res.forEach((community)=> {
+            var dom = `
+                <a href="#" class="list-group-item" data-community-id="${community.communityId}">
+                    <h4 class="list-group-item-heading">${sanitizer.sanitize(community.communityName)}</h4>
+                    <p class="list-group-item-text">${sanitizer.sanitize(community.introduction)}</p>
 
+                </a>
+                `;
+            $list.append(dom);
+          });
+        })
+        .catch((error)=> {
+          console.log(error);
+        })
     }
   });
 
